442ac117a99bfbbdd389554a08554447c0270657
Oskix <oskar.polak@akademiakodu.pl>
2018-10-13 11:00:47 +0200

    Initial commit

---
+++ src/main/java/pl/oskarpolak/chatex/ChatexApplication.java
@@ -0,0 +1,12 @@
+package pl.oskarpolak.chatex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatexApplication.class, args);
+	}
+}
