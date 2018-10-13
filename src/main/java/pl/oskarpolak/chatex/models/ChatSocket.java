178069f363a0f980de73b03172ec56e9ea431562
Oskix <oskar.polak@akademiakodu.pl>
2018-10-13 11:46:41 +0200

    kkk

---
+++ src/main/java/pl/oskarpolak/chatex/models/ChatSocket.java
@@ -0,0 +1,23 @@
+package pl.oskarpolak.chatex.models;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+@EnableWebSocket
+public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(this, "/chat").setAllowedOrigins("*");
+    }
+
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("Że ktoś połaczył sie z serwerem");
+    }
+}
