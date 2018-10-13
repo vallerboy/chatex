41aa75ea147df3e5c6d4e05c122346db2e9bc9f1
Oskix <oskar.polak@akademiakodu.pl>
2018-10-13 12:49:18 +0200

    kkk

--- src/main/java/pl/oskarpolak/chatex/models/ChatSocket.java
+++ src/main/java/pl/oskarpolak/chatex/models/ChatSocket.java
@@ -1,6 +1,7 @@
 package pl.oskarpolak.chatex.models;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -8,22 +9,34 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 @EnableWebSocket
 public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+
+    private List<WebSocketSession> users = new ArrayList<>();
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry.addHandler(this, "/chat").setAllowedOrigins("*");
     }
 
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        users.remove(session);
+    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("Że ktoś połaczył sie z serwerem");
+        users.add(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println(message.getPayload());
+        for (WebSocketSession user : users) {
+            user.sendMessage(message);
+        }
     }
 }
